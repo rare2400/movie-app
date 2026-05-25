5d2fbf3902f01b6c3feaa909b370a7975e5f049e
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-25 10:25:35 +0200

    import header and footer components with router view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import AppHeader from '@/components/Header.vue';
+import AppFooter from '@/components/Footer.vue';
+import { RouterView } from 'vue-router';
+</script>
 
 <template>
-<h1>Filmlistan</h1>
+    <div id="app">
+        <AppHeader />
+        <main class="main-content">
+            <RouterView />
+        </main>
+        <AppFooter />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#app {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.main-content {
+    flex: 1;
+}
+</style>
