83c6dfa59cc5a0355e5c00c1beac05390f0cee72
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-25 10:23:24 +0200

    add router with routes

---
+++ src/router/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomwView from '@/views/HomeView.vue'
+import MovieListView from '@/views/MovieListView.vue'
+import AssignmentView from '@/views/AssignmentView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [{
+        path: "/",
+        name: "hem",
+        component: HomwView
+    },
+    {
+        path: "/filmlista",
+        name: "filmlista",
+        component: MovieListView
+    },
+    {
+        path: "/om-vue",
+        name: "om vue",
+        component: AssignmentView
+    }]
+})
+
+export default router
