b667447bdae604340a89100fe9691bfd2ace024f
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-25 10:21:30 +0200

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <footer>
+        <p>Copyright &copy; 2026 Ramona Reinholdz</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: #111;
+    border-top: 1px solid #505050;
+    margin-top: 1em;
+    color: #fff;
+    text-align: center;
+    padding: 1rem;
+}
+</style>
