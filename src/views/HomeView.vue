b49106a0b85fa48c44142ac81a8cbd6dd7b3fd87
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-25 11:28:34 +0200

    create HomeView component with banner

---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="banner">
+        <img src="@/assets/banner.png" alt="filmlistan banner" width="500">
+    </div>
+
+    <h2>Filmlistan</h2>
+    <p>Välkommen till filmlistan</p>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.banner {
+    width: 100%;
+}
+
+.banner img {
+    max-width: 100%;
+    width: 700px;
+}
+
+h2 {
+    text-align: center;
+    font-size: 2em;
+}
+
+p {
+    text-align: center;
+    font-size: 1.2em;
+}
+
+@media screen and (min-width: 700px) {
+    .banner {
+        max-width: 1220px;
+        margin: 0 auto;
+    }
+
+    .banner img {
+        max-width: 100%;
+        width: 1220px;
+    }
+}
+</style>
