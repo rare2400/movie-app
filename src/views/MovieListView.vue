d3dc1633531ac6684ee15777736585e359639353
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-25 21:52:06 +0200

    add comments and style the table responsive

--- src/views/MovieListView.vue
+++ src/views/MovieListView.vue
@@ -35,13 +35,15 @@ import { ref, onMounted } from 'vue';
 
 const movies = ref([]);
 
+// Get movies when component is mounted
 onMounted(() => {
     getMovies();
 });
 
+// Fetch movies from API
 const getMovies = async () => {
     try {
-        const res = await fetch('https://fastify-movieapi.onrender.com/movies');
+        const res = await fetch("https://fastify-movieapi.onrender.com/movies");
 
         if (res.ok) {
             const data = await res.json();
@@ -53,6 +55,7 @@ const getMovies = async () => {
     }
 }
 
+// Delete movie by ID
 const deleteMovie = async (id) => {
     try {
         const res = await fetch(`https://fastify-movieapi.onrender.com/movies/${id}`, {
@@ -82,5 +85,49 @@ p {
     font-size: 1.2em;
 }
 
+/* Table */
+.table-container {
+    margin: 1rem auto;
+    overflow-x: auto;
+}
 
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th {
+    text-align: left;
+    padding: 0.75rem;
+    border-bottom: 2px solid #ddd;
+    font-size: 0.9rem;
+}
+
+/* Hide cells on small screens */
+.hide-mobile {
+    display: none;
+}
+
+.status {
+    display: none;
+}
+
+@media screen and (min-width: 500px){
+    /* Display status column on medium screens and up */
+    .status {
+        display: table-cell;
+    }
+}
+
+@media screen and (min-width: 700px) {
+    /* Display length and rating columns on larger screens */
+    .hide-mobile {
+        display: table-cell;
+    }
+
+    .table-container {
+        max-width: 800px;
+        margin: 2rem auto;
+    }
+}
 </style>
